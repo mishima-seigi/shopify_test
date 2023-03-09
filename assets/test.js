9be98b5e8c8fe73bda4bd0515a1e458ba02c789c
seigi-mishima <seigi.mishima@divx.co.jp>
2023-03-09 15:45:23 +0900

    js追加変更

--- assets/test.js
+++ assets/test.js
@@ -1,6 +1,6 @@
 
 //アコーディオンをクリックした時の動作
-$('.title').on('click', function() {//タイトル要素をクリックしたら
+window.onload = $('.title').on('click', function() {//タイトル要素をクリックしたら
   var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
   $(findElm).slideToggle();//アコーディオンの上下動作
     
@@ -12,7 +12,7 @@ $('.title').on('click', function() {//タイトル要素をクリックしたら
 });
 
 //ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
-$(window).on('load', function(){
+window.onload = $(window).on('load', function(){
   $('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
   $(".open").each(function(index, element){ //openクラスを取得
     var Title =$(element).children('.title'); //openクラスの子要素のtitleクラスを取得
